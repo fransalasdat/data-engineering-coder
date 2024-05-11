46d5d731569a003b0af30190363ad5f42771f9d5
fransalasdat <136126358+fransalasdat@users.noreply.github.com>
2024-05-11 17:36:54 -0300

    Update Main.py
    Se optimizo el el codigo en funcion de las sugerencias de los profesores.

--- Main.py
+++ Main.py
@@ -1,11 +1,18 @@
+# Se modifico el nombre de los campos de la tabla a snake case
+# Se agrego una columna update para dejar sentado la fecha de la ultima actualizacion
+# Traer Season dinamicamente tomando la fecha actual
+# Se aislaron las contraseñas en un archivo config.py
+
 import json  # Importar el módulo json para manejar datos JSON
 import psycopg2  # Importar la biblioteca psycopg2 para interactuar con PostgreSQL (Redshift)
 import requests  # Importar la biblioteca requests para hacer solicitudes HTTP 
+from datetime import datetime #Importar datetime para obtener la fecha actual
+import config
 
 # Datos de conexión a Redshift
 dbname = 'data-engineer-database'
 user = 'francoesalas_coderhouse'
-password = 'eHgW2401vJ'
+password = config.PWD_REDSHIFT
 host = 'data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com'
 port = '5439'
 
@@ -19,29 +26,41 @@ cur = conn.cursor()
 # Cabeceras para la solicitud HTTP a la API
 headers = {
     'x-rapidapi-host': "v3.football.api-sports.io",
-    'x-rapidapi-key': "bbf04fb6aac9b84535835019b45f55a2"
+    'x-rapidapi-key': config.API_KEY
 }
 
 try:
     # Crear la tabla en Redshift si no existe
     cur.execute("""
         CREATE TABLE IF NOT EXISTS current_standings (
-            Rank INT,
-            Team VARCHAR(255),
-            Points INT,
-            Goals_Difference INT,
-            League_id VARCHAR(255),
-            Form VARCHAR(255)
+            rank INT,
+            team VARCHAR(255),
+            points INT,
+            goals_difference INT,
+            league_id VARCHAR(255),
+            form VARCHAR(255),
+            updated DATE
         )
     """)
+    #Obtener Fecha y hora
+    now = datetime.now()
+    date_updated = now.date()
+    #Obtener Año de la temporada dinamicamente
+    year_now = now.year
+    month_now = now.month
 
-    # Insertar los datos de las ligas en la tabla
+    if month_now<=7:
+        year_season = year_now-1
+    else:
+        year_season = year_now
+        
+    # Iteramos e Insertamos los datos de las ligas en la tabla
     for league_id in big_five_leagues:
-        # Eliminar datos previos de la liga en la tabla
-        cur.execute(f"DELETE FROM current_standings WHERE League_id = '{league_id}'")
+        # Eliminar datos previos de esa liga en la tabla
+        cur.execute(f"DELETE FROM current_standings WHERE league_id = '{league_id}'")
         
         # Hacer la solicitud HTTP a la API para obtener los datos de la tabla de posiciones
-        url = f"https://v3.football.api-sports.io/standings?league={league_id}&season=2023"
+        url = f"https://v3.football.api-sports.io/standings?league={league_id}&season={year_season}"
         response = requests.get(url, headers=headers)
         result_dict = response.json()
         
@@ -59,10 +78,10 @@ try:
                 form = team["form"]
                 # Insertar los datos en la tabla current_standings en Redshift
                 cur.execute("""
-                    INSERT INTO current_standings (Rank, Team, Points, Goals_Difference, League_id, Form)
-                    VALUES (%s, %s, %s, %s, %s, %s)
-                """, (rank, team_name, points, goals_diff, league_id, form))
-
+                    INSERT INTO current_standings (rank, team, points, goals_difference, league_id, form,updated)
+                    VALUES (%s, %s, %s, %s, %s, %s, TO_DATE(%s, 'YYYY-MM-DD'))
+                """, (rank, team_name, points, goals_diff, league_id, form,date_updated))
+    
     # Confirmar la transacción
     conn.commit()
     print("Los datos se han cargado exitosamente en la tabla current_standings en Redshift.")
